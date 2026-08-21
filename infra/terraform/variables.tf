ebaee2539e3df403daf6269bea72d0da7176a4dc
Richard Alvarez <richie.alvarez94@gmail.com>
2026-08-20 21:08:34 -0400

    Finished game logic. Moving onto Terraform infra

---
+++ infra/terraform/variables.tf
@@ -0,0 +1,35 @@
+variable "prefix" {
+    description = "Short name prefix for resources"
+    type = string
+    default = "eastus"
+}
+
+variable "location" {
+  description = "Azure region to deploy into."
+  type = string
+  default = "eastus"
+}
+
+variable "resource_group_name" {
+  description = "Name of the resource group to create."
+  type = string
+  default = "galaga-rg"
+}
+
+variable "acr_name" {
+  description = "Globally-unique ACR name (letters and numbers only, 5-50 characters.)"
+  type = string
+  default = "galagaacrrac09011994"
+}
+
+variable "node_count" {
+  description = "Number of worker VMs (nodes) in the AKS pool."
+  type = number
+  default = 2 # no single point of failure
+}
+
+variable "node_vm_size" {
+  description = "VM size for AKS nodes."
+  type = string
+  default = "Standard_B2s" # small and cheap. Excellent for demo game
+}
